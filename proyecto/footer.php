c783df8dee54bcf9a951708d527c1425190219be
Cataprogramm <65853401+Cataprogramm@users.noreply.github.com>
2021-02-21 23:00:25 +0100

    Add files via upload

---
+++ proyecto/footer.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+</head>
+<body>
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ proyecto/functions.php
@@ -0,0 +1,29 @@
+<?php
+//Prefijo común
+function temadecata() {
+  wp_enqueue_style('diw-style', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'temadecata');
+
+//Incluyo Bootstrap CSS
+function bootstrap_css() {
+	wp_enqueue_style( 'bootstrap_css', 
+  					get_stylesheet_directory_uri() . '/css/bootstrap.min.css', 
+  					array(), 
+  					'4.1.3'
+  					); 
+}
+add_action( 'wp_enqueue_scripts', 'bootstrap_css');
+
+
+//Incluyo Bootstrap JS
+function bootstrap_js() {
+	wp_enqueue_script( 'bootstrap_js', 
+  					get_stylesheet_directory_uri() . '/js/bootstrap.min.js', 
+  					array('jquery'), 
+  					'4.1.3', 
+  					true); 
+}
+add_action( 'wp_enqueue_scripts', 'bootstrap_js');
+
+?>
